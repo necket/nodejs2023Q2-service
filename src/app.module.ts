809d80d9adacf32aeea9c803b530695b7fe4167f
necket <nik.sherbakov.01@gmail.com>
2023-07-26 01:06:51 +0300

    remove "hello world" controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { TrackModule } from './modules/track/track.module';
 import { ArtistModule } from './modules/artist/artist.module';
@@ -16,9 +14,7 @@ import { FavoritesModule } from './modules/favorites/favorites.module';
     AlbumModule,
     FavoritesModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({
