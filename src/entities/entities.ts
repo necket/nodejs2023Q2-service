fe8e3c24e613993a8ffa931af08a40e711f2281a
necket <nik.sherbakov.01@gmail.com>
2023-08-07 21:12:20 +0300

    artist to entity

--- src/entities/entities.ts
+++ src/entities/entities.ts
@@ -1,3 +1,4 @@
+import { Artist } from 'src/modules/artist/artist.entity';
 import { User } from 'src/modules/user/user.entity';
 
-export const entities = [User];
+export const entities = [User, Artist];
